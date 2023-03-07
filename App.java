866a002f24fa31acdd1110733809fe9522b8fab1
Marcos <martinezdiezmarcos7@gmail.com>
2023-03-07 12:26:08 +0100

    ahora pone mi residencia

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Marcos Carbajal");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("07/03/2023");
+    System.out.println("Leon");
   }
   
 }
